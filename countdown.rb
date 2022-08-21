42f15e8f89e3063cc09bccce652af94c16f9eae8
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-08-21 20:08:26 +0000

    Completed lab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+# require "pry"
+def countdown (number)
+  while number > 0  
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+# countdown(5);
+# binding.pry
 
-def countdown
+def countdown_with_sleep (number)
+  while number > 0  
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
